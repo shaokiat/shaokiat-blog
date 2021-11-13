ddfb1889180c51ad722ce6349a4923dda79a9fd9
shaokiat <shaokiat@gmail.com>
2021-11-13 23:24:11 +0800

    Add dynamic programming and graph

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
           },
           {
             label: "Resume",
-            href: "https://shaokiat.github.io/shaokiat-blog/resume.pdf",
+            href: "https://shaokiat.github.io/shaokiat-blog/pdf/resume.pdf",
             position: "right",
           },
         ],
